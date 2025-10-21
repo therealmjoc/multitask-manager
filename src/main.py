f803b24450200b09f4a64d46a607dad29fe238b2
Matthew <matthewj.oconnor01@gmail.com>
2025-10-21 16:29:52 +0100

    Dates now utilise datetime.date objects

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import os
 import time
+import datetime
+from datetime import date
+
+# TODO Source system date to fetch all tasks due in the next seven days
+# TODO Fully design task according to spec
+# TODO Save tasks to a csv file
 
 name = "Matthew"
 tasks = []
 
 currentHour = (((time.time())/60)/60)%24 + 1
+
+x = datetime.date.today()
 
 
 class task:
@@ -13,14 +21,13 @@ class task:
         self.dueDate = dueDate
 
     def __str__(self):
-        return f"{self.taskName}({self.dueDate})"
+        return f"{self.taskName} ({self.dueDate})"
 
 
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def homeScreen():
-    print(currentHour + 1)
     if (currentHour < 12):
         greeting = "Good morning"
     elif(currentHour > 12 and currentHour < 17):
@@ -28,23 +35,40 @@ def homeScreen():
     else:
         greeting = "Hi"
     print(greeting+" "+name+", you have",len(tasks),"tasks due today.")
+    printTaskTable()
 
+    print("[1] Add new task    [2] View All Tasks    [3] Settings    [4] Quit")
+
+def printTaskTable():
+    print("Your week at a glance:")
     print("+------------------------+")
     for task in tasks:
-        print("| ",task,"  |")
+        print("| ",task)
     print("+------------------------+")
 
 def newTask():
     thisName = input("Task Name: ")
-    thisDate = input("Input Date (DD/MM/YY): ")
+    thisDay = int(input("Input Day:"))
+    thisMonth = int(input("Input Month:"))
+    thisYear = int(input("Input Year:"))
+
+    thisDate = date(thisYear, thisMonth, thisDay)
+
     thisTask = task(thisName, thisDate)
     tasks.append(thisTask)
 
 def main():
     homeScreen()
-    input("Add new task?")
-    newTask()
-    clearScreen()
-    homeScreen()
+    while True:
+        choice = int(input())
+        match choice:
+            case 1:
+                newTask()
+            case 4:
+                quit()
+            case _:
+                print("Invalid operation")
+        clearScreen()
+        homeScreen()
 
 main()
